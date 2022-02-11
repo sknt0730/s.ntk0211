6d58b3a5efc7e446e88d6e548d752ba26b8a1ef6
sknt0730 <zm19568@josai.ac.jp>
2022-02-11 05:14:12 +0000

    4345

--- myjs.js
+++
@@ -1,4 +0,0 @@
-function clickBtn1() {
-    const t1=document.getElementById("name1").value;
-    document.getElementById("span1").textContent=t1;
-}
---
+++ quiz1.js
@@ -0,0 +1,6 @@
+function quiz1() {
+    if(document.getElementById('ans1').value == 'おすしがたべたい') {
+    const a =document.getElementById('11');
+console.log(a);
+    }
+}
